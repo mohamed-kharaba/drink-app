353f219a48827cfc9b6bc6338398ed02cacf7964
Mohamed Kharaba <msk_h85@hotmail.com>
2025-03-04 16:55:25 +0300

    Add structure for project and add About , error pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,44 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { About, Landing, Cocktail, NewsLetter, HomeLayout, Error } from "./pages";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <HomeLayout />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Landing />,
+            },
+            {
+                path: "cocktail",
+                element: <Cocktail />,
+            },
+            {
+                path: "newsletter",
+                element: <NewsLetter />,
+            },
+            {
+                path: "about",
+                element: <About />,
+                children: [
+                    {
+                        path: "company",
+                        element: <h2>Our Company</h2>,
+                    },
+                    {
+                        path: "person",
+                        element: <h2>Mohamed Kharaba</h2>,
+                    },
+                ],
+            },
+        ],
+    },
+]);
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+    return <RouterProvider router={router}></RouterProvider>;
 };
 export default App;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,15 @@
+import { Link, Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <Navbar />
+            <section className="page">
+                <Outlet />
+            </section>
+        </div>
+    );
+};
+
+export default HomeLayout;
