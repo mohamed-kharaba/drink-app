580c9b6e0c145f84c46d37da4fcf0c401a9ac8d9
Mohamed Kharaba <msk_h85@hotmail.com>
2025-03-08 15:45:51 +0300

    Add Single cocktail functions

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
 import Wrapper from "../assets/wrappers/CocktailPage";
@@ -21,10 +21,72 @@ const Cocktail = () => {
     if (isLoading) return <div className="loading"></div>;
     if (error) return <div className="error"> error fetching cocktail</div>;
 
-    const singleDrink = data.drinks[0];
-    console.log(singleDrink);
+    if (!data || !data.drinks) return null;
 
-    return <h1>Cocktail</h1>;
+    const singleDrink = data.drinks[0];
+    // console.log(singleDrink);
+
+    const {
+        strDrink: name,
+        strDrinkThumb: image,
+        strAlcoholic: info,
+        strCategory: category,
+        strGlass: glass,
+        strInstructions: instructions,
+    } = singleDrink;
+
+    const validIngredient = Object.keys(singleDrink)
+        .filter((key) => key.startsWith("strIngredient") && singleDrink[key] !== null)
+        .map((key) => singleDrink[key]);
+
+    // console.log(validIngredient);
+
+    return (
+        <Wrapper>
+            <header>
+                <Link to="/" className="btn">
+                    back home
+                </Link>
+                <h3>{name}</h3>
+            </header>
+            <div className="drink">
+                <img src={image} alt={name} className="img" />
+                <div className="drink-info">
+                    <p>
+                        <span className="drink-data">Name :</span>
+                        {name}
+                    </p>
+                    <p>
+                        <span className="drink-data">Category :</span>
+                        {category}
+                    </p>
+                    <p>
+                        <span className="drink-data">Info :</span>
+                        {info}
+                    </p>
+                    <p>
+                        <span className="drink-data">Glass :</span>
+                        {glass}
+                    </p>
+                    <p>
+                        <span className="drink-data">instructions :</span>
+                        {instructions}
+                    </p>
+                    <p>
+                        <span className="drink-data">Ingredient :</span>
+                        {validIngredient.map((item, index) => {
+                            return (
+                                <span className="ing" key={item}>
+                                    {item}
+                                    {index < validIngredient.length - 1 ? "," : ""}
+                                </span>
+                            );
+                        })}
+                    </p>
+                </div>
+            </div>
+        </Wrapper>
+    );
 };
 
 export default Cocktail;
